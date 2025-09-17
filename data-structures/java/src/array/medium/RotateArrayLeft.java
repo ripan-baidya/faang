b57c5524d0d984009cab4df83c407d989aedb638
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-09-17 09:55:37 +0530

    rotate array left

---
+++ data-structures/java/src/array/medium/RotateArrayLeft.java
@@ -0,0 +1,47 @@
+package array.medium;
+
+import java.util.Arrays;
+
+/**
+ * @author Ripan Baidya
+ * @date 30-07-2025
+ *
+ * Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps,
+ * where d is a positive integer. Do the mentioned change in the array in place.
+ * Note: Consider the array as circular.
+ *
+ * Example:
+ * Input: arr[] = [1, 2, 3, 4, 5], d = 2
+ * Output: [3, 4, 5, 1, 2]
+ * Explanation: when rotated by 2 elements, it becomes 3 4 5 1 2.
+ */
+public class RotateArrayLeft {
+    // reverse array
+    private void reverse(int[] nums, int s, int e){
+        while(s <= e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+
+            s ++;
+            e --;
+        }
+    }
+    public void rotateArr(int arr[], int d) {
+        if (arr == null || arr.length == 0 || d <= 0) return; // No rotation needed
+        int n = arr.length;
+        d = d % n; // to handle cases where d >= n
+
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+
+    public static void main(String[] args) {
+        var obj = new RotateArrayLeft();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        obj.rotateArr(arr, 2);
+        System.out.println("Rotated array is: " + Arrays.toString(arr));
+    }
+}
