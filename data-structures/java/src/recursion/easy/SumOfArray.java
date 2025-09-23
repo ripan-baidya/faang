0e96fc593a1c93139e679f4b4c100bc92ff01108
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-09-23 12:54:31 +0530

    sum of array elements

---
+++ data-structures/java/src/recursion/easy/SumOfArray.java
@@ -0,0 +1,42 @@
+package recursion.easy;
+
+/**
+ * @author Ripan Baidya
+ * @date 23/09/25
+ *
+ * You are given an integer array arr[]. The task is to find the sum of it.
+ *
+ * Examples:
+ * Input: arr[] = [1, 2, 3, 4]
+ * Output: 10
+ * Explanation: 1 + 2 + 3 + 4 = 10.
+ */
+public class SumOfArray {
+    /**
+     * To solve this problem we will use a helper function, which will take
+     * the array and its size. We will stop whenever there is a single element
+     * in the array. And while backtracking, we will add the 'n-1'-th element
+     * to get the total sum of the array.
+     * time: O(n), space: O(n)
+     */
+    int arySumHelper(int[] arr, int n) {
+        // Base Case
+        if (n == 1) return arr[0];
+
+        return arySumHelper(arr, n-1) + arr[n-1];
+    }
+
+    int arraySum(int arr[]) {
+        int n = arr.length;
+        return arySumHelper(arr, n);
+    }
+
+    static void main() {
+        var obj = new SumOfArray();
+
+        int[] arr = {1, 2, 3};
+        int arraySum = obj.arraySum(arr);
+
+        System.out.println("sum of all array elements: " + arraySum);
+    }
+}
