746ead60c3ffe3d861c33b34c4758b6c09ba60a9
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-10-01 23:32:47 +0530

    Binary Search

--- data-structures/java/src/array/easy/BinarySearch.java
+++ data-structures/java/src/array/easy/BinarySearch.java
@@ -9,33 +9,47 @@ package array.easy;
  * return -1. You must write an algorithm with O(log n) runtime complexity.
  *
  * Example:
+ *
  * Input: nums = [-1,0,3,5,9,12], target = 9
  * Output: 4
  * Explanation: 9 exists in nums and its index is 4
  */
 public class BinarySearch {
+    /**
+     * We will take two pointers start and end to keep track of the search space. we will iterate
+     * till start <= end. at this point we will calculate mid and check if mid element is equal to
+     * target element or not. if element is found return its index else return -1.
+     * if mid element is less than target element, we will search in the right half else search in
+     * the left half.
+     *
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     */
     public int search(int[] arr, int key) {
-        int n = arr.length;
-        int start = 0, end = n-1;
+        int n = arr.length; // array length
+        int start = 0, end = n-1; // start and end pointers
 
         while (start <= end) {
             // calculate mid
             int mid = start + (end-start)/2;
 
             if (arr[mid] == key) {
+                // mid is the target element
                 return mid;
             } else if (arr[mid] < key) {
+                // search in the right half
                 start = mid+1;
             } else {
+                // search in the left half
                 end = mid-1;
             }
         }
-
+        // target not found
         return -1;
     }
 
     public static void main(String[] args) {
-        BinarySearch obj = new BinarySearch();
+        var obj = new BinarySearch();
 
         int[] arr = {-1,0,3,5,9,12};
         int target = 9;
