1ffde962cc3d3e20c1fb04e07aafb071a4dddb1b
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-10-04 14:50:11 +0530

    combination sum

--- data-structures/java/src/recursion/medium/CombinationSum.java
+++ data-structures/java/src/recursion/medium/CombinationSum.java
@@ -7,13 +7,12 @@ import java.util.List;
  * @author Ripan Baidya
  * @date 25/09/25
  *
- * Given an array of distinct integers candidates and a target integer target, return a list of
- * all unique combinations of candidates where the chosen numbers sum to target. You may return
- * the combinations in any order.
- * The same number may be chosen from candidates an unlimited number of times. Two combinations
- * are unique if the frequency of at least one of the chosen numbers is different.
- * The test cases are generated such that the number of unique combinations that sum up to target
- * is less than 150 combinations for the given input.
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique
+ * combinations of candidates where the chosen numbers sum to target. You may return the combinations  in
+ * any order. The same number may be chosen from candidates an unlimited number of times. Two combinations
+ * are unique if the frequency of at least one of the chosen numbers is different. The test cases are
+ * generated such that the number of unique combinations that sum up to target is less than 150 combinations
+ * for the given input.
  *
  * Example:
  * Input: candidates = [2,3,6,7], target = 7
@@ -25,40 +24,44 @@ import java.util.List;
  */
 public class CombinationSum {
     /**
-     * Approach is very simple
-     * 1. At every index, either pick the number (if ≤ target, and stay at same index since repeats
-     * allowed) or skip it (move to next index).
-     * 2. When target == 0, store the current path as a valid combination.
-     * 3. Backtrack by removing the last picked number before exploring other choices.
+     * Approach: Backtracking
+     * The idea is to explore all possible combinations of candidates array. At each step, we have two choices:
+     *  1. Pick the current element and move forward.
+     *  2. Skip (don’t pick) the current element and move forward.
      *
-     * Time Complexity: O(2^n * k)
-     * Space Complexity: O(target/min(candidates)) + O(output size)
+     * We stop the recursion when we either reach the length of the candidates array or the current sum exceeds
+     * the target. If we find a valid combination (where the sum of the elements in the combination is equal to
+     * the target), we add it to the 'ans' list. We use backtracking to remove the last element from the 'path'
+     * and explore other possibilities when we exceed the target or reach the end of the candidates array.
+     *
+     * Time Complexity: O(2^n * k), where n is the length of the candidates array and k is the target. This is
+     * because we need to explore all possible combinations.
+     * Space Complexity: O(target/min(candidates)) + O(output size), where output size is the number of combinations
+     * that sum up to the target.
      */
-    private void helper(int idx, int[] candidates, int target,
-                        List<Integer> list, List<List<Integer>> ans) {
-        // Base case
-        // Whenever we found target we would add it to the 'ans' list and return.
-        if (target == 0) {
-            ans.add(new ArrayList<>(list));
+    private void dfs(int idx, int[] candidates, int target, int currentSum, List<Integer> path,
+                     List<List<Integer>> ans) {
+        // Base Case 1: Whenever we found target we would add it to the 'ans' list and return.
+        if (currentSum == target) {
+            ans.add(new ArrayList<>(path));
             return;
         }
-        // When we reach the length we will return
-        if (idx == candidates.length) return;
+        // Base Case 2: When we reach the length or currentSum exceeds target we will return
+        if (idx == candidates.length || currentSum > target) return;
 
-        // pick current element if it doesn’t exceed target
-        if (candidates[idx] <= target) {
-            list.add(candidates[idx]);
-            helper(idx, candidates, target - candidates[idx], list, ans);
-            list.remove(list.size() - 1); // backtrack
-        }
+        path.add(candidates[idx]);
+        dfs(idx, candidates, target, target+candidates[idx], path, ans);
+        path.removeLast(); // remove while backtrack
 
-        // skip current element
-        helper(idx + 1, candidates, target, list, ans);
+        // check for next index
+        dfs(idx+1, candidates, target, currentSum, path, ans);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        helper(0, candidates, target, new ArrayList<>(), ans);
+
+        // parameters: idx, candidates, target, currentSum, path, ans
+        dfs(0, candidates, target, 0, new ArrayList<>(), ans);
         return ans;
     }
 
